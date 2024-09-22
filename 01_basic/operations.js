9d60eca9e59331a325ba4159fecbc385da61b750
Mansi5164 <156164354+Mansi5164@users.noreply.github.com>
2024-09-22 14:05:10 +0000

    commit

---
+++ 01_basic/operations.js
@@ -0,0 +1,9 @@
+//console.log(+true);  //1
+//console.log(+false);   //0
+
+let num1 , num2 , num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;    
+console.log(gameCounter);    //101
