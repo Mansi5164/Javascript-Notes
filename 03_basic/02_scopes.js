7a29ea612abcf2d3bde93493e433a6ecb3bff02d
Mansi5164 <156164354+Mansi5164@users.noreply.github.com>
2024-09-27 08:52:55 +0000

    commit

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -2,7 +2,19 @@ let a = 5;      // working in block scope
 const b = 20;
 var c = 30;     //var is global datatype
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
+function one(){
+    const username = "hitesh";
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two();
+}
+one();
