d87fb5716dbc733e9fe81c8dae837ae9190505ed
Mansi5164 <156164354+Mansi5164@users.noreply.github.com>
2024-09-29 07:55:33 +0000

    commit

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,19 @@
+//Imediately Invoked function Expression (IIFE)
+
+(function chai(){
+    //named iife
+    console.log(`DB CONNECTED!`);
+})();
+
+// (/*function defintion*/) (/*Execution call*/)
+
+//IIFE USE : jo function immediately execute hojaye + global scope ke pollution se problem hoti hai kayi baar toh global scope ke jo bhi variables hai ya declaration ha unke pollution ko htane ke liye hmne iife ka use kiya 
+
+( () => {
+    console.log(`DB connected!!`);
+})();
+
+
+( (name) => {
+    console.log(`DB connected two ${name}`);
+})('Hitesh');
